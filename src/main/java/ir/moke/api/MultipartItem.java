dc565dc4d79ef4f30cbd44e328d9ea87c3e68727
mah454 <mah454@gmail.com>
2024-01-26 18:41:51 +0330

    Change UploadFileApi (not completed)

---
+++ src/main/java/ir/moke/api/MultipartItem.java
@@ -0,0 +1,31 @@
+package ir.moke.api;
+
+public class MultipartItem {
+    private String name ;
+    private String fileName;
+    private byte[] bytes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+}
--- src/main/java/ir/moke/api/UploadFileApi.java
+++ src/main/java/ir/moke/api/UploadFileApi.java
@@ -1,18 +1,16 @@
 package ir.moke.api;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import ir.moke.utils.HttpUtils;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Base64;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UploadFileApi implements HttpHandler {
-    private static final Gson gson = new Gson();
 
     public UploadFileApi() {
         System.out.println("UploadFileApi: singleton object");
@@ -22,59 +20,66 @@ public class UploadFileApi implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
         System.out.println("Request method: " + method);
-        if (method.equalsIgnoreCase("post")) {
+        Headers requestHeaders = exchange.getRequestHeaders();
+        String contentType = requestHeaders.get("content-type").get(0);
+        List<MultipartItem> itemList = new ArrayList<>();
+        if (contentType.startsWith("multipart/form-data") && method.equalsIgnoreCase("post")) {
+            /*
+             * --------------------------7ef8140c649d8c54
+             * Content-Disposition: form-data; name="file"; filename="file.img"
+             * Content-Type: application/octet-stream
+             *
+             * [File bytes]
+             * */
             byte[] allBytes = exchange.getRequestBody().readAllBytes();
-            System.out.println("Start fetch data, Request length: " + allBytes.length);
-            if (allBytes.length > 0) {
-                try {
-                    String content = new String(allBytes);
-                    JsonObject jsonObject = gson.fromJson(content, JsonObject.class);
-                    if (jsonObject.has("file")) {
-                        String base64File = jsonObject.get("file").getAsString();
-                        storeFile(base64File);
-                        System.out.println("Upload Finished");
-                    } else {
-                        sendResponse(exchange, "Invalid json".getBytes(), 400);
+            StringBuilder content = new StringBuilder();
+            List<Byte> fileByes = new ArrayList<>();
+            MultipartItem multipartItem = new MultipartItem();
+            for (byte b : allBytes) {
+                content.append((char) b);
+                if (content.toString().startsWith("--------") && content.toString().endsWith("\r\n")) {
+                    content.setLength(0);
+                } else if (content.toString().startsWith("Content-Disposition") && content.toString().endsWith("\r\n")) {
+                    for (String p : content.toString().split(";")) {
+                        String[] kv = p.split("=");
+                        if (kv.length > 1) {
+                            if (kv[0].trim().equalsIgnoreCase("name")) multipartItem.setName(kv[1].trim().substring(1).substring(0, kv[1].trim().length() - 2));
+                            if (kv[0].trim().equalsIgnoreCase("filename")) multipartItem.setFileName(kv[1].trim().substring(1).substring(0, kv[1].trim().length() - 2));
+                        }
                     }
-                } catch (Exception e) {
-                    sendResponse(exchange, "Invalid Json".getBytes(), 500);
-                    return;
+                    content.setLength(0);
+                } else if (content.toString().startsWith("Content-Type") && content.toString().endsWith("\r\n")) {
+                    content.setLength(0);
+                } else if (content.toString().startsWith("\r\n")) {
+                    fileByes.add(b);
+                    if (content.toString().endsWith("\r\n-----")) {
+                        byte[] bytes = new byte[fileByes.size() - 8];
+                        for (int i = 1; i < fileByes.size() - 7; i++) {
+                            bytes[i - 1] = fileByes.get(i);
+                        }
+
+                        multipartItem.setBytes(bytes);
+                        itemList.add(multipartItem);
+                        multipartItem = new MultipartItem();
+                        content.setLength(0);
+                        fileByes.clear();
+                        content.append("-----");
+                    }
                 }
             }
+
+            for (MultipartItem item : itemList) {
+                saveFile(item);
+            }
         }
-        setResponseCORSHeaders(exchange);
-        sendResponse(exchange, "".getBytes(), 204);
+
+        HttpUtils.sendResponse(exchange, "".getBytes(), 204);
     }
 
-    private static void storeFile(String b64) {
-        byte[] fileContent = Base64.getDecoder().decode(b64);
-        File uploadedFile = new File("/tmp/uploadedFile");
-        try (FileOutputStream fos = new FileOutputStream(uploadedFile)) {
-            fos.write(fileContent);
-            fos.flush();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void sendResponse(HttpExchange exchange, byte[] response, int statusCode) {
-        try {
-            int contentLength = statusCode == 204 ? -1 : response.length;
-            exchange.sendResponseHeaders(statusCode, contentLength);
-            OutputStream responseBody = exchange.getResponseBody();
-            responseBody.write(response);
-            responseBody.flush();
-            responseBody.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void setResponseCORSHeaders(HttpExchange httpExchange) {
-        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-        if (httpExchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
-            httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
-            httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type,Authorization");
-        }
+    private static void saveFile(MultipartItem item) throws IOException {
+        FileOutputStream fos = new FileOutputStream("/tmp/" + item.getFileName());
+        fos.write(item.getBytes());
+        fos.flush();
+        fos.close();
     }
 }
